c0dc0bb7113a5d062e373ad63417b8010919bdd9
Gabriel Flores <gabriel.flores@nestrixcorp.com>
2015-03-05 12:51:45 -0600

    Refactoring code

--- js/jquery.table.js
+++ js/jquery.table.js
@@ -1,4 +1,4 @@
-var newNode = function(tag, content, attributes, data){
+function newNode (tag, content, attributes, data){
     var element = document.createElement(tag);
     
     if(content !== undefined && content != null){
@@ -24,6 +24,13 @@ var newNode = function(tag, content, attributes, data){
     }
     return element;
 };
+function removeNode (element, childName) {
+    children = element.getElementsByTagName(childName);
+    for(var i = 0; i < children.length; i++){
+        element.removeChild(children[i]);
+    }
+}
+
 (function($){
     $.fn.SimpleTable = function(options){
         var base_settings = $.extend({
@@ -36,124 +43,155 @@ var newNode = function(tag, content, attributes, data){
             rePag: true,
             enum: true,
             headClass : "info",
-            noRowsMessage: "No records to display"
+            noRowsMessage: "No records to display",
+            searchForm: false,
+            searchOn: "submit" //submit, write
         }, options);
-        //Go to specific page
-        var gotoPage = function(table, settings, page){
-            settings.page = page;
-            settings.header = false;
-            loadTable(table, settings);
-        }
-        var loadTable = function(table, settings){
-            //Remove currents rows
-            var bodies = table.getElementsByTagName('tbody');
-            for(var i = 0; i < bodies.length; i++){
-                table.removeChild(bodies[i]);
-            }
-            //Create new body
-            var body = document.createElement('tbody');
-            //Create table header if not exists
-            var heads;
-            if(settings.header){
-                heads = table.getElementsByTagName('thead');
-                for(var i = 0; i < heads.length; i++){
-                    table.removeChild(heads[i]);
-                }
-                var columns = settings.columns;
-                
-                var headTr = document.createElement('tr');
-                headTr.className = settings.headClass;
-                if(settings.enum){
-                    var tdhc = newNode('td','#');
-                    headTr.appendChild(tdhc);
-                }
-                for(var i = 0; i < columns.length; i++){
-                    field = columns[i];
-                    tdh = newNode('td', field);
-                    headTr.appendChild(tdh);
-                }
-                var head = newNode('thead', headTr);
-            }
-            //Add pagination if is set
-            var tableData;
-            var start = 0;
-            var end;
-            //Pagination settings
-            if(settings.pagination){
-                start = (settings.page - 1)*settings.size;
-                end = start+settings.size;
-                tableData = settings.data.slice(start, end);
-                settings.noPages = (settings.data.length / settings.size).toFixed(0);
-            }
-            else
-                tableData = settings.data;
-            
-            //Pagination HTML
-            if(settings.rePag && settings.pagination && tableData.length > 0){
-                
-                tableData = settings.data.slice(start, end);
-                
-                var pages = newNode('ul',null,[{name: "class", value: "pagination"}]);
-                
-                var pagesContainer = document.createElement('div');
-                table.parentElement.appendChild(pagesContainer);
-                pagesContainer.appendChild(pages);
-                
-                var firstPageLink = newNode('a', newNode('span','«'), null, [{name: "page", value: 1}]);
-                $(firstPageLink).click(function(){
-                    gotoPage(table, settings, this.dataset.page);
-                });
-                var firstPage = newNode('li', firstPageLink);
-                
-                pages.appendChild(firstPage);
-                
-                for(var i = 1; i<= settings.noPages; i++){
-                    var pageLink = newNode('a', i, null, [{name: "page", value: i}]);
-                    
-                    $(pageLink).click(function(){
-                        gotoPage(table, settings, this.dataset.page);
-                    });
-                    var pageItem = newNode('li', pageLink);
-                    pages.appendChild(pageItem);
-                }
-                var lastPageLink = newNode('a', newNode('span','»'), null, [{name: "page", value: settings.noPages}])
-                $(lastPageLink).click(function(){
-                    gotoPage(table, settings, this.dataset.page);
-                });
-                var lastPage = newNode('li', lastPageLink);
-                pages.appendChild(lastPage);
-                settings.rePag = false;
-            }
-            //Add Data Rows
-            if(tableData.length == 0){
-                var colspan = columns.length + (settings.enum ? 1 : 0);
-                noRows = newNode('tr', newNode('td',settings.noRowsMessage,[{name:"colspan", value: colspan}]));
-                body.appendChild(noRows);
-            }
-            else
-                for(var j = 0; j < tableData.length; j++){
-                    var row = tableData[j];
-                    var tbRow = document.createElement('tr');
-                    //If enum rows is required
-                    if(settings.enum){
-                        var tdc = newNode('td', start+j+1);
-                        tbRow.appendChild(tdc);
-                    }
-                    for(var k = 0; k < row.length; k++){
-                        value = row[k];
-                        td = newNode('td', value);
-                        tbRow.appendChild(td);
-                    }
-                    body.appendChild(tbRow);
-                }
-            if(settings.header)
-                table.appendChild(head);
-            table.appendChild(body);
-        }
+        
+        
         return this.each(function(){
             var newSettings = $.extend({}, base_settings);
             newSettings.data = base_settings.data;
-            loadTable(this, newSettings);
+            
+            var table;
+            if(this.tagName.toLowerCase() === 'table')
+                table = this;
+            else{
+                table = newNode('table',null,[{name:'class', value: 'table table-hover'}]);    
+                this.appendChild(table);
+            }
+            
+            var builder = new TableBuilder(table, newSettings);
+            
+            var director = new Table(builder);
+            
+            return director.build();
         });
     }
 }(jQuery))
+var Table = function (builder) {
+    this.builder = builder;
+    var self = this;
+    this.build = function () {
+        self.builder.clear();
+        self.builder.buildHead();
+        builder.buildBody();
+        builder.buildPagination();
+        return self.builder.get();
+    }
+}
+var TableBuilder = function (table, settings) {
+    this.table = table;
+    this.settings = settings;
+    var self = this;
+    //Remove currents rows
+    this.clear = function () {
+        removeNode( self.table, 'tbody');
+        if(settings.header)
+            removeNode( self.table, 'thead');
+    }
+    //Create table header if not exists
+    this.buildHead = function () {
+        if (settings.header) {
+            
+            var columns =  self.settings.columns;
+            var headTr = newNode('tr', '', [{name: "class", value: self.settings.headClass}]); 
+            
+            if (self.settings.enum) headTr.appendChild(newNode('td','#'));
+            
+            for (var i = 0; i < columns.length; i++) headTr.appendChild(newNode('td', columns[i]));
+            
+            var head = newNode('thead', headTr);
+            
+            self.table.appendChild(head);
+        }
+    }
+    this.getPaginationSettings = function () {
+        //Add pagination if is set
+            var pageData;
+            var start = 0;
+            var end;
+            //Pagination settings
+            if (self.settings.pagination) {
+                start = (self.settings.page - 1)*self.settings.size;
+                end = start+self.settings.size;
+                self.settings.noPages = (self.settings.data.length / self.settings.size).toFixed(0);
+                pageData = self.settings.data.slice(start, end);
+                
+            }
+            else
+                pageData = self.settings.data;
+        
+        return {
+            pageData: pageData, 
+            start: start, 
+            end: end
+        };
+    }
+    this.buildBody = function () {
+        //Create new body
+        var body = document.createElement('tbody');
+        var paginationSettings = self.getPaginationSettings();
+        
+        //Add Data Rows
+        if (paginationSettings.pageData.length == 0) {
+            
+            var colspan = self.settings.columns.length + (self.settings.enum ? 1 : 0);
+            
+            noRows = newNode('tr', 
+                                newNode('td',settings.noRowsMessage,[{name:"colspan", value: colspan}])
+                            );
+            body.appendChild(noRows);
+        }
+        else {
+            for (var j = 0; j < paginationSettings.pageData.length; j++) {
+                
+                var row = paginationSettings.pageData[j];
+                var tbRow = document.createElement('tr');
+                
+                //If enum rows is required
+                if (self.settings.enum) tbRow.appendChild(newNode('td', paginationSettings.start+j+1));
+                
+                for (var k = 0; k < row.length; k++) tbRow.appendChild(newNode('td', row[k]));
+                
+                body.appendChild(tbRow);
+            }
+        }
+        self.table.appendChild(body);
+    }
+    this.buildPagination = function () {
+        var pageData = self.getPaginationSettings().pageData;
+        if(settings.rePag && settings.pagination && pageData.length > 0){
+            
+            var pagesContainer = document.createElement('div');
+            
+            var pages = newNode('ul',null,[{name: "class", value: "pagination"}]);
+            pagesContainer.appendChild(pages);
+            //First Page
+            pages.appendChild(self.createPageItem(newNode('span','«'), 1));
+            //Page Numbers
+            for(var i = 1; i<= settings.noPages; i++)  pages.appendChild(self.createPageItem(i, i));
+            //Last Page
+            pages.appendChild(self.createPageItem(newNode('span','»'), self.settings.noPages));
+            //Do not create pagination every table load
+            self.settings.rePag = false;
+            //Add the pagination to the parent
+            self.table.parentElement.appendChild(pagesContainer);
+        }
+    }
+    this.createPageItem = function(content, page){
+        var link = newNode('a', content, null, [{name: "page", value: page}]);
+        
+        $(link).click(function(){
+            self.settings.page = page;
+            self.settings.header = false;
+            self.clear();
+            self.buildBody();
+        });
+        
+        return newNode('li', link);
+    }
+    this.get = function(){
+        return self.table;
+    }
+}
